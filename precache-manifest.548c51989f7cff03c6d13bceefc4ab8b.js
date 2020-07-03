d2f885c871c1685d2c0dcf33760833f6bcc5c852
Slpixe <slpixe@gmail.com>
2020-07-04 00:08:58 +0100

    Updates

---
+++ precache-manifest.548c51989f7cff03c6d13bceefc4ab8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06553020866094db562fb19b2b96fcc8",
+    "url": "/Video-Comments-Search/index.html"
+  },
+  {
+    "revision": "3e2459e0be43b79d60dd",
+    "url": "/Video-Comments-Search/static/css/main.6579c26d.chunk.css"
+  },
+  {
+    "revision": "3104913359b90e08d765",
+    "url": "/Video-Comments-Search/static/js/2.5e9ca07c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Video-Comments-Search/static/js/2.5e9ca07c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2459e0be43b79d60dd",
+    "url": "/Video-Comments-Search/static/js/main.7791b026.chunk.js"
+  },
+  {
+    "revision": "fd82744fd70c84302fa4",
+    "url": "/Video-Comments-Search/static/js/runtime-main.5ff35b1a.js"
+  }
+]);
