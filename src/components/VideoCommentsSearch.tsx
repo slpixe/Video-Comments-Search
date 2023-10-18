8e2ad3cfbf3a210ae3f40971f4a5fa3e49d3fe7c
Slpixe <slpixe@gmail.com>
2023-10-18 23:48:06 +0000

    prettier working in vscode now

--- src/components/VideoCommentsSearch.tsx
+++ src/components/VideoCommentsSearch.tsx
@@ -1,21 +1,13 @@
-import { FormControl, FormHelperText, Input, InputLabel } from '@mui/material'
+import { FormControl, FormHelperText, Input, InputLabel } from '@mui/material';
 
 export const VideoCommentsSearch: React.FC = () => {
   return (
-        <div className={'root'}>
-            <FormControl>
-
-
-              
-
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
-                              <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-</FormControl>
-        </div>
-  )
-}
-
-
-
-
+    <div className={'root'}>
+      <FormControl>
+        <InputLabel htmlFor="my-input">Email address</InputLabel>
+        <Input id="my-input" aria-describedby="my-helper-text" />
+        <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+      </FormControl>
+    </div>
+  );
+};
