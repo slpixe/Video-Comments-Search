e58daa778e8d2e3aa34fc91287983eea3e31922f
Slpixe <slpixe@gmail.com>
2023-10-19 01:27:05 +0000

    some state

--- src/components/VideoCommentsSearch.tsx
+++ src/components/VideoCommentsSearch.tsx
@@ -11,8 +11,27 @@ import {
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import BigList from './BigList.tsx';
+import { useState } from 'react';
 
 export const VideoCommentsSearch: React.FC = () => {
+  const [state, setState] = useState({
+    textBox1: '',
+    textBox2: '',
+  });
+
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    console.log('====submit');
+    e.preventDefault();
+    console.log(state);
+  };
+
   return (
     <div className={'root'}>
       {/* <FormControl row> */}
@@ -25,16 +44,37 @@ export const VideoCommentsSearch: React.FC = () => {
       </Button> */}
       {/* </FormControl> */}
       <Toolbar style={{ display: 'flex', justifyContent: 'center' }}>
-        <TextField helperText="e.g. kJQP7kiw5Fk" id="ident" label="Youtube video ID" style={{ marginRight: '10px' }} />
-        <TextField helperText="e.g. song" id="query" label="Search term" style={{ marginRight: '10px' }} />
-        <Button
-          variant="contained"
-          size="large"
-          endIcon={<SearchIcon />}
-          style={{ alignSelf: 'start', paddingTop: '14px', paddingBottom: '14px' }}
-        >
-          Send
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            helperText="e.g. kJQP7kiw5Fk"
+            id="ident"
+            label="Youtube video ID"
+            style={{ marginRight: '10px' }}
+            type="text"
+            name="textBox1"
+            value={state.textBox1}
+            onChange={handleChange}
+          />
+          <TextField
+            helperText="e.g. song"
+            id="query"
+            label="Search term"
+            style={{ marginRight: '10px' }}
+            type="text"
+            name="textBox2"
+            value={state.textBox2}
+            onChange={handleChange}
+          />
+          <Button
+            variant="contained"
+            size="large"
+            endIcon={<SearchIcon />}
+            style={{ alignSelf: 'start', paddingTop: '14px', paddingBottom: '14px' }}
+            type="submit"
+          >
+            Send
+          </Button>
+        </form>
       </Toolbar>
       <BigList />
     </div>
